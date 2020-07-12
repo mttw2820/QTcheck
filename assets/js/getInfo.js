5c44fc0d198c1a15e172d7f13bc54e6f7c354316
mttw2820 <kooong2820@sookmyung.ac.kr>
2020-07-12 18:53:38 +0900

    create qt_week buttons, add JSON code

---
+++ assets/js/getInfo.js
@@ -0,0 +1,75 @@
+// onclick functions for main pages
+// on campus button clicked
+
+var campusSymbol = new Map();
+campusSymbol['동국'] = "&#127819;";
+campusSymbol['배화'] = "&#127800;";
+campusSymbol['성균'] = "&#128171;";
+campusSymbol['상명'] = "&#9752;";
+campusSymbol['숙명'] = "&#10052;";
+campusSymbol['숭의'] = "&#127803;";
+
+function campus_onclick(cam){
+	var new_campus = document.getElementById(cam);	// 클릭된 캠퍼스
+	var welcome_space = document.getElementById("welcometxt");
+	var sid_block = document.getElementById("sid_block");
+	var name_block = document.getElementById("name_block");
+	
+	if(campus == null){	// 첫 선택
+		var welcometext = "이름이 뭐에요?";
+		var sym = campusSymbol[cam];
+		welcome_space.innerText = welcometext;
+		new_campus.innerHTML = "&nbsp;" + sym + "&nbsp;";
+		new_campus.className = "button primary";
+		campus = cam;
+		
+		welcome_space.style.display = "block";
+		sid_block.style.display = "block";
+		name_block.style.display = "block";
+		
+	} else {	// 중복 또는 취소
+		if(campus != cam) { // 중복
+			alert("캠퍼스는 하나만 선택해주세요."); 
+		}
+		else { // 취소 - 기존에 선택되었던 캠퍼스 취소
+			campus = null;
+			new_campus.innerText = cam;
+			new_campus.className = "button";
+			welcome_space.style.display = "none";
+			sid_block.style.display = "none";
+			name_block.style.display = "none";
+		}		
+	}
+}
+
+function sidTyped(s_id){
+	var typed_sid = document.getElementById(s_id);
+	sid = typed_sid.value;
+}
+
+function nameTyped(name_id){
+	var typed_name = document.getElementById(name_id);
+	name = typed_name.value;
+}
+
+
+// on qt week button clicked
+function qtweek_onclick(btn_id){
+	var clicked_week = document.getElementById(btn_id);
+	
+	if(qt_week == null){	// 첫 선택
+		clicked_week.innerHTML = "&nbsp&nbsp" + "&#127775" + "&nbsp;";
+		clicked_week.className = "button primary";
+		qt_week = btn_id;	
+	} else {	// 중복 또는 취소
+		if(qt_week != btn_id) { // 중복
+			alert("한 주차씩 인증해주세요!"); 
+		}
+		else { // 취소 - 기존에 선택되었던 캠퍼스 취소
+			qt_week = null;
+			clicked_week.innerText = btn_id;
+			clicked_week.className = "button";
+		}		
+	}
+}
+
--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,30 +1,29 @@
+var jq = document.createElement('script');
+jq.src = "https://code.jquery.com/jquery-3.1.0.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
 
-//$('#submit_qt').click(function(){
-function qt_submit_onclick(){
-	alert("submit_qt clicked");
-	submit_index = "QT";
-	var qt_msg = $('#qt_message');
-	var week = "1";
-	alert("got msg");
+
+function send_data(){
+	alert("func start");
 	$.ajax({
-		type : "GET",
-		url : "https://script.google.com/macros/s/AKfycbxMz41wYe1ozJKiK9Zp4LfHmzKhrY1gtH5SOQa4q0OE1T5VxEM/exec",
-		data: {
-			"submit_index": submit_index,
-			"Campus": campus,
-			"SID": sid,
-			"Name": name,
-			"Week": week,
-			"Message": "test"
-		},
-		success: function(response){
-			alert("입력 완료");
+		url: "https://script.google.com/macros/s/AKfycbzn7wdgjii-t7nNFX2MQUiV_s4hxx6wWFhWyd2u2bs63IubQQA/exec",
+		data: {'sheet': 'Data', 'nickname': 'test_nickname', 'name': 'test_name'},
+		type: "GET",
+		dataType: "JSON",
+		success : function(data){
+			alert('SUCCESS');
 		},
 		error: function(request, status, error){
-			isLoading(false);
-			alert("code: " + request.status + "\n" + "error: " + error);
-			alert(request.responseText);
+			alert("code: " + request.status + "\nmessage: " + request.responseText + "\nerror: " + error);
 		}
-	});	
-//});
+	})
+	.done(function(request){
+		alert("DONE");
+	})
+	.fail(function(request, status, errorThrown){
+		alert("code: " + request.status + "\nmessage: " + request.responseText + "\nerror: " + error);
+	})
+	.always(function(xhr, status){
+		alert("ALWAYS");
+	});
 }
